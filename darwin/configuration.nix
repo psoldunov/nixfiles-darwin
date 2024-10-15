71a47f7a6e69af7fb211536ea52e05ff88eeda8c
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2024-10-15 16:05:15 +0300

    rebuild commit 15/10/2024 16:05:15

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -19,6 +19,9 @@ in {
     package = pkgs.nixVersions.git;
     settings = {
       experimental-features = "nix-command flakes";
+      substituters = [
+        "https://cache.nixos.org/"
+      ];
     };
   };
 
